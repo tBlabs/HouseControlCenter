dd5e504ef40660f0cf4635478259ec1b9e3e065e
tB <wendro@wp.pl>
2018-10-19 23:25:50 +0200

    after audit

--- src/Main.ts
+++ src/Main.ts
@@ -1,5 +1,3 @@
-import { IRunMode } from './services/runMode/IRunMode';
-import { ILogger } from './services/logger/ILogger';
 import { injectable, inject } from 'inversify';
 import { Types } from './IoC/Types';
 import { IStartupArgs } from './services/environment/IStartupArgs';
@@ -8,17 +6,11 @@ import { IStartupArgs } from './services/environment/IStartupArgs';
 export class Main
 {
     constructor(
-        @inject(Types.IStartupArgs) private _args: IStartupArgs,
-        @inject(Types.ILogger) private _log: ILogger,
-        @inject(Types.IRunMode) private _runMode: IRunMode)
+        @inject(Types.IStartupArgs) private _args: IStartupArgs)
     { }
 
-    public async Run(): Promise<void>
+    public async Start(): Promise<void>
     {
-        this._log.Info('Main.Run', 'Starting in "' + this._runMode.Current + '" mode with args:', this._args.Args); // Don't Try it with "npm run run --foo bar" or "npm run run -- --foo bar", it won't work! Call script directly: "tsc || /bin/startup.js --foo bar"
-      
-        /* Put your code here */
-        /* And then run `npm run serve` */
-        /* Don't forget to set your environment variables (`.env` file) */
+        console.log('hello');
     }
 }
